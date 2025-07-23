95c2f9a210138b0ef04a7a03df0095be61566ce4
Isaac Odrosky <92752062+isaacodrosky@users.noreply.github.com>
2025-07-23 13:47:36 -0400

    Calculation added

--- app.js
+++ app.js
@@ -2,20 +2,36 @@
 // DONE V1 collect user input (vers 1: by 'calculate' click
 // vers 2: on final input)
 // error message if inputs left empty
-// calculate tip amounts/total
-// display calculations
+// DONE calculate tip amounts/total
+// DONE display calculations
 // reset function - all inputs back to 'empty'
 
 const billTotal = document.getElementById('bill')
 const tipPercent = document.querySelector('input[type="radio"]:checked')
 const numberPeople = document.getElementById('people')
 const calcBtn = document.getElementById('calc-btn')
+const resetBtn = document.getElementById('reset-btn')
+// keep consistent html vs js naming (change id names below?)
+const tipPerPersonDisplay = document.getElementById('tipPerPersonDisplay')
+const totalPerPersonDisplay = document.getElementById('totalPerPersonDisplay')
 
 calcBtn.addEventListener('click', (e) => {
-    //convert bill, percentage, people value to float type var
+    //convert bill, percentage, people value to numbers
     const bill = parseFloat(billTotal.value)
     const percent = parseFloat(tipPercent.value)
-    const people = parseFloat(numberPeople.value)
+    const people = parseInt(numberPeople.value)
 
-    
+    //calculate per person tip and total
+    const tipPerPerson = (bill * percent) / people
+    const totalPerPerson = ((bill / people) + tipPerPerson)
+
+    // update dom to display these values
+    tipPerPersonDisplay.innerText = `${tipPerPerson}`
+    totalPerPersonDisplay.innerText = `${totalPerPerson}`
+})
+
+resetBtn.addEventListener('click', (e) => {
+    billTotal.value = ''
+    tipPercent.checked = false
+    numberPeople.value = ''
 })
